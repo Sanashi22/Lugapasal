3e5b1c915e226486894077a2111fbec56230c1ab
Sanashi22 <shakya.santona@gmail.com>
2023-11-26 21:14:03 -0800

    user signup, serializer

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from .views import UserLoginView, UserLogOutAPIView
+from django.urls import path,include
+from .views import UserLoginView, UserLogOutAPIView, SignUpViews
+from rest_framework.routers import DefaultRouter
+
+router= DefaultRouter()
+router.register(r'signup', SignUpViews, basename='signup')
 
 app_name='account'
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/',UserLogOutAPIView.as_view(), name='logout')
+    path('logout/',UserLogOutAPIView.as_view(), name='logout'),
+    path('', include(router.urls))
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
-from .serializers import UserloginSerializer
+from .serializers import UserloginSerializer, UserSignUpSerializer
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate, logout
 from rest_framework.authtoken.models import Token
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 class UserLoginView(APIView):
@@ -34,6 +35,10 @@ class UserLogOutAPIView(APIView):
     def post(request):
         logout(request)
         return Response({'message':'User Logout Successfully.'}, status=status.HTTP_200_OK)
+    
+class SignUpViews(ModelViewSet):
+    serializer_class= UserSignUpSerializer
+    queryset=User.objects.all()
 
 
 
